e777974f170df43826a8a123e6726b6a1d76600a
myusrilh <yusrilhasriansyah@gmail.com>
2020-12-14 01:34:00 +0700

    update parkirassistant

--- src/ParkirAssistant/app/src/main/java/org/proyek/parkirassistant/DBContract.java
+++ src/ParkirAssistant/app/src/main/java/org/proyek/parkirassistant/DBContract.java
@@ -10,19 +10,19 @@ package org.proyek.parkirassistant;
 
 
 public class DBContract {
-    public static final String SERVER_LOGIN_URL = "http://192.168.1.2/cobaparkir/api/Login_Api/auth";
-    public static final String SERVER_SIGNUP_URL = "http://192.168.1.2/cobaparkir/api/Signup_Api/auth";
-    public static final String SERVER_UPDATE_URL = "http://192.168.1.2/cobaparkir/api/Updateprofile_Api/auth";
-    public static final String SERVER_BOOKING_URL = "http://192.168.1.2/cobaparkir/api/Booking_Api/cek";
-    public static final String SERVER_RIWAYAT_TRANSAKSI_URL = "http://192.168.1.2/cobaparkir/api/Transaksi_Api/riwayat";
-    public static final String SERVER_INSERT_BOOKING_URL = "http://192.168.1.2/cobaparkir/api/Booking_Api/input";
-    public static final String SERVER_DELETE_BOOKING_URL = "http://192.168.1.2/cobaparkir/api/Booking_Api/delete";
-    public static final String SERVER_UPDATE_PARKIR_BOOKING_URL = "http://192.168.1.2/cobaparkir/api/Booking_Api/changestatus";
-    public static final String SERVER_CEK_ENTRY_URL = "http://192.168.1.2/cobaparkir/api/Entry_Api/cekentry";
-    public static final String SERVER_CEK_ENTRY_PELANGGAN_URL = "http://192.168.1.2/cobaparkir/api/Entry_Api/cekentrypelanggan";
-    public static final String SERVER_CEK_BOOKING_PELANGGAN_URL = "http://192.168.1.2/cobaparkir/api/Booking_Api/cekbookingpelanggan";
-    public static final String SERVER_INPUT_NO_BOOKING_ENTRY_URL = "http://192.168.1.2/cobaparkir/api/Entry_Api/entrynobooking";
-    public static final String SERVER_INPUT_BOOKING_ENTRY_URL = "http://192.168.1.2/cobaparkir/api/Entry_Api/entrybooking";
-    public static final String SERVER_CHECKOUT_ENTRY_URL = "http://192.168.1.2/cobaparkir/api/Entry_Api/checkout";
-    public static final String SERVER_INPUT_TRANSAKSI_URL = "http://192.168.1.2/cobaparkir/api/Transaksi_Api/inputtransaksi";
+    public static final String SERVER_LOGIN_URL = "http://192.168.1.3/cobaparkir/api/Login_Api/auth";
+    public static final String SERVER_SIGNUP_URL = "http://192.168.1.3/cobaparkir/api/Signup_Api/auth";
+    public static final String SERVER_UPDATE_URL = "http://192.168.1.3/cobaparkir/api/Updateprofile_Api/auth";
+    public static final String SERVER_BOOKING_URL = "http://192.168.1.3/cobaparkir/api/Booking_Api/cek";
+    public static final String SERVER_RIWAYAT_TRANSAKSI_URL = "http://192.168.1.3/cobaparkir/api/Transaksi_Api/riwayat";
+    public static final String SERVER_INSERT_BOOKING_URL = "http://192.168.1.3/cobaparkir/api/Booking_Api/input";
+    public static final String SERVER_DELETE_BOOKING_URL = "http://192.168.1.3/cobaparkir/api/Booking_Api/delete";
+    public static final String SERVER_UPDATE_PARKIR_BOOKING_URL = "http://192.168.1.3/cobaparkir/api/Booking_Api/changestatus";
+    public static final String SERVER_CEK_ENTRY_URL = "http://192.168.1.3/cobaparkir/api/Entry_Api/cekentry";
+    public static final String SERVER_CEK_ENTRY_PELANGGAN_URL = "http://192.168.1.3/cobaparkir/api/Entry_Api/cekentrypelanggan";
+    public static final String SERVER_CEK_BOOKING_PELANGGAN_URL = "http://192.168.1.3/cobaparkir/api/Booking_Api/cekbookingpelanggan";
+    public static final String SERVER_INPUT_NO_BOOKING_ENTRY_URL = "http://192.168.1.3/cobaparkir/api/Entry_Api/entrynobooking";
+    public static final String SERVER_INPUT_BOOKING_ENTRY_URL = "http://192.168.1.3/cobaparkir/api/Entry_Api/entrybooking";
+    public static final String SERVER_CHECKOUT_ENTRY_URL = "http://192.168.1.3/cobaparkir/api/Entry_Api/checkout";
+    public static final String SERVER_INPUT_TRANSAKSI_URL = "http://192.168.1.3/cobaparkir/api/Transaksi_Api/inputtransaksi";
 }
